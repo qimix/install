92f469131f8aa972da26327f316c902ed9197ae3
Vitaliy Murzin <murzinvit@yandex.ru>
2024-04-03 00:33:09 +0500

    delete Data

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static StringBuilder stringBuilder = new StringBuilder();
 
     public static void main(String[] args) {
-        makeDir();
-        writeLog();
+        List<String> dirs = new ArrayList<String>();
+                dirs.add("C://Games");
+                dirs.add("C://Games//src");
+                dirs.add("C://Games//res");
+                dirs.add("C://Games//savegames");
+                dirs.add("C://Games//temp");
+                dirs.add("C://Games//src//main");
+                dirs.add("C://Games//src//test");
+                dirs.add("C://Games//res//drawables");
+                dirs.add("C://Games//res//vectors");
+                dirs.add("C://Games//res//icons");
+
+        List<String> files = new ArrayList<String>();
+                files.add("C://Games//src//main//Main.java");
+                files.add("C://Games//src//main//Utils.java");
+                files.add("C://Games//temp//temp.txt");
+
+        makeDir(dirs, files);
+        writeLog("C://Games//temp//temp.txt");
     }
 
-    public static void makeDir(){
-        for (String i : new Data().getDirs()) {
+    public static void makeDir(List<String> dirs,List<String> files){
+        for (String i : dirs) {
             if (new File(i).mkdir()) {
                 stringBuilder.append("Директория " + i + " создана" + "\n");
             } else {
@@ -20,7 +39,7 @@ public class Main {
         }
 
         try {
-            for (String i : new Data().getFiles()) {
+            for (String i : files) {
                 if (new File(i).createNewFile()) {
                     stringBuilder.append("Файл " + i + " создан" + "\n");
                 } else {
@@ -32,8 +51,8 @@ public class Main {
         }
     }
 
-    public static void writeLog(){
-        try(FileWriter fileWriter = new FileWriter(new File("C://Games//temp//temp.txt"))) {
+    public static void writeLog(String logPath){
+        try(FileWriter fileWriter = new FileWriter(new File(logPath))) {
             fileWriter.write(stringBuilder.toString().toCharArray());
         } catch (IOException e) {
             System.out.println(e.getMessage());
